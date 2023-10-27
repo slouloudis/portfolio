bf1ca74a838e6e047952c3eccbf6ba61ea258c3a
Sam Louloudis <samanthaleelouloudis@gmail.com>
2023-10-27 14:00:30 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
     <p>My name is sam and I'm a web dev. I think.</p>
+    <br />
+    <p>I kinda know react, and next and some other things and maybe oneday something exciting. 
+    </p>
     </>
   )
 }
